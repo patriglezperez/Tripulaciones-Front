8a6f3f008ee5202e11e31cc033630cfd3352f1e9
josemsb92 <josemsb92@gmail.com>
2022-05-28 23:43:47 +0200

    RegisterForm created, missing validation

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,6 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 const RegisterForm = () => {
-  return <>Hola</>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  //It will post the form data to the backend using a helper
+  /* const handlePostForm = async(data) => {
+    postUser(data, jwt);
+  } */
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        //handlePostForm(data)
+        reset();
+      })}
+    >
+      <input
+        {...register("email", { required: "This field cannot be empty" })}
+        placeholder="Email"
+      />
+      <p>{errors.email?.message}</p>
+      <input
+        {...register("name", { required: "This field cannot be empty" })}
+        placeholder="Name"
+      />
+      <p>{errors.name?.message}</p>
+      <input
+        {...register("surname", { required: "This field cannot be empty" })}
+        placeholder="Surname"
+      />
+      <p>{errors.surname?.message}</p>
+      <input
+        {...register("address", { required: "This field cannot be empty" })}
+        placeholder="Address"
+      />
+      <p>{errors.address?.message}</p>
+      <input
+        {...register("password", { required: "This field cannot be empty" })}
+        placeholder="Password"
+      />
+      <p>{errors.password?.message}</p>
+      <button>Submit</button>
+    </form>
+  );
 };
 export default RegisterForm;
