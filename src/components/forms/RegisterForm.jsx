9f65f0b144b6ffad22f13935d131a8ab0d63a061
josemsb92 <josemsb92@gmail.com>
2022-05-30 00:27:36 +0200

    Validation applied  to the form, except for Address input

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 const RegisterForm = () => {
   const {
@@ -7,10 +7,30 @@ const RegisterForm = () => {
     reset,
     formState: { errors },
   } = useForm();
+
+  const required = {
+    required: "This field cannot be empty",
+  };
+  const emailValidation = {
+    pattern: {
+      value:
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: "invalid email address",
+    },
+  };
+  const passwordValidation = {
+    pattern: {
+      value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+      message:
+        "Your password is not strong enough, it must contain numbers, Uppercase and LowerCase.",
+    },
+  };
+
   //It will post the form data to the backend using a helper
   /* const handlePostForm = async(data) => {
     postUser(data, jwt);
   } */
+
   return (
     <form
       onSubmit={handleSubmit((data) => {
@@ -19,30 +39,52 @@ const RegisterForm = () => {
       })}
     >
       <input
-        {...register("email", { required: "This field cannot be empty" })}
+        type="email"
+        {...register("email", { ...required, ...emailValidation })}
         placeholder="Email"
       />
-      <p>{errors.email?.message}</p>
+      {errors.email?.type === "required" && <p>{errors.email.message}</p>}
+      {errors.email?.type === "pattern" && <p>{errors.email.message}</p>}
+
       <input
-        {...register("name", { required: "This field cannot be empty" })}
+        {...register("name", {
+          ...required,
+          minLength: { value: 2, message: "Name is too short" },
+        })}
         placeholder="Name"
       />
-      <p>{errors.name?.message}</p>
+      {errors.name?.type === "required" && <p>{errors.name.message}</p>}
+      {errors.name?.type === "minLength" && <p>{errors.name.message}</p>}
       <input
-        {...register("surname", { required: "This field cannot be empty" })}
+        {...register("surname", {
+          ...required,
+          minLength: { value: 2, message: "Surname is too short" },
+        })}
         placeholder="Surname"
       />
-      <p>{errors.surname?.message}</p>
+      {errors.surname?.type === "required" && <p>{errors.surname.message}</p>}
+      {errors.surname?.type === "minLength" && <p>{errors.surname.message}</p>}
+
+      <input {...register("address", { ...required })} placeholder="Address" />
+      {errors.address?.type === "required" && <p>{errors.address.message}</p>}
+
       <input
-        {...register("address", { required: "This field cannot be empty" })}
-        placeholder="Address"
-      />
-      <p>{errors.address?.message}</p>
-      <input
-        {...register("password", { required: "This field cannot be empty" })}
+        type="password"
+        {...register("password", {
+          ...required,
+          minLength: {
+            value: 8,
+            message: "password length must be 8 or greater",
+          },
+          ...passwordValidation,
+        })}
         placeholder="Password"
       />
-      <p>{errors.password?.message}</p>
+      {errors.password?.type === "required" && <p>{errors.password.message}</p>}
+      {errors.password?.type === "pattern" && <p>{errors.password.message}</p>}
+      {errors.password?.type === "minLength" && (
+        <p>{errors.password.message}</p>
+      )}
       <button>Submit</button>
     </form>
   );
