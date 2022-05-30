6a8f0552d323391872ad80b27a1b6ff8af1d1aef
josemsb92 <josemsb92@gmail.com>
2022-05-30 21:32:21 +0200

    yup schema applied to RegisterForm

--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+const schema = yup.object({
+  email: yup
+    .string()
+    .required()
+    .email()
+    .matches(emailRegex, "Email is not a valid email"),
+  name: yup.string().required().min(2),
+  surname: yup.string().required().min(2),
+  address: yup.string().required().min(2),
+  password: yup
+    .string()
+    .required()
+    .min(8, "password is to short, must be minimun 8 characters")
+    .matches(
+      passwordRegex,
+      "password needs at list an Uppercase, lowercase and number"
+    ),
+});
+
 const RegisterForm = () => {
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
-
-  const required = {
-    required: "This field cannot be empty",
-  };
-  const emailValidation = {
-    pattern: {
-      value:
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      message: "invalid email address",
-    },
-  };
-  const passwordValidation = {
-    pattern: {
-      value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
-      message:
-        "Your password is not strong enough, it must contain numbers, Uppercase and LowerCase.",
-    },
-  };
+  } = useForm({ resolver: yupResolver(schema) });
 
   //It will post the form data to the backend using a helper
   /* const handlePostForm = async(data) => {
@@ -38,53 +45,17 @@ const RegisterForm = () => {
         reset();
       })}
     >
-      <input
-        type="email"
-        {...register("email", { ...required, ...emailValidation })}
-        placeholder="Email"
-      />
-      {errors.email?.type === "required" && <p>{errors.email.message}</p>}
-      {errors.email?.type === "pattern" && <p>{errors.email.message}</p>}
+      <input type="email" {...register("email")} placeholder="Email" />
+      {<p>{errors.email?.message}</p>}
+      <input {...register("name")} placeholder="Name" />
+      {<p>{errors.name?.message}</p>}
+      <input {...register("surname")} placeholder="Surname" />
+      {<p>{errors.surname?.message}</p>}
+      <input {...register("address")} placeholder="Address" />
+      {<p>{errors.address?.message}</p>}
 
-      <input
-        {...register("name", {
-          ...required,
-          minLength: { value: 2, message: "Name is too short" },
-        })}
-        placeholder="Name"
-      />
-      {errors.name?.type === "required" && <p>{errors.name.message}</p>}
-      {errors.name?.type === "minLength" && <p>{errors.name.message}</p>}
-      <input
-        {...register("surname", {
-          ...required,
-          minLength: { value: 2, message: "Surname is too short" },
-        })}
-        placeholder="Surname"
-      />
-      {errors.surname?.type === "required" && <p>{errors.surname.message}</p>}
-      {errors.surname?.type === "minLength" && <p>{errors.surname.message}</p>}
-
-      <input {...register("address", { ...required })} placeholder="Address" />
-      {errors.address?.type === "required" && <p>{errors.address.message}</p>}
-
-      <input
-        type="password"
-        {...register("password", {
-          ...required,
-          minLength: {
-            value: 8,
-            message: "password length must be 8 or greater",
-          },
-          ...passwordValidation,
-        })}
-        placeholder="Password"
-      />
-      {errors.password?.type === "required" && <p>{errors.password.message}</p>}
-      {errors.password?.type === "pattern" && <p>{errors.password.message}</p>}
-      {errors.password?.type === "minLength" && (
-        <p>{errors.password.message}</p>
-      )}
+      <input type="password" {...register("password")} placeholder="Password" />
+      {<p>{errors.password?.message}</p>}
       <button>Submit</button>
     </form>
   );
