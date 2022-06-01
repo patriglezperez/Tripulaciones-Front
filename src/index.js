02a4cbae326e5548f07b31e4e2d748fdd8c02162
josemsb92 <josemsb92@gmail.com>
2022-06-01 11:53:28 +0200

    imported sentry and applied to the app index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./context/authContext";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
+
+Sentry.init({
+  dsn: "https://59baf01118be4f0380efae4558916848@o1170633.ingest.sentry.io/6264339",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
