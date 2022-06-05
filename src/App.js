69989ddb3d67c5f26d968202245308b406a46e5b
josemsb92 <josemsb92@gmail.com>
2022-06-05 18:04:18 +0200

    testing react-big-calendar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import RegisterForm from "./components/forms/RegisterForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import Error from "./components/Error/Error";
 import * as Sentry from "@sentry/react";
+import moment from "moment";
+const localizer = momentLocalizer(moment);
 function App() {
   const error = "Page not found";
   const error404 = "Error 404";
-
+  const myEventsList = [
+    {
+      title: "event1",
+      start: new Date("June 04, 2022 05:12:00"),
+      end: new Date("June 04, 2022 10:12:00"),
+      allDay: false,
+      resource: "any resource the event may be related to",
+    },
+  ];
   return (
     <div>
+      <Calendar
+        className="calendario"
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+      />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<RegisterForm />} />
