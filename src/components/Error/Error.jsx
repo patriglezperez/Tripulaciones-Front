2023b3d6c1d4ae9844cecc08c608c4432c5f0e57
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-06-11 18:33:39 +0200

    add css basic

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -5,15 +5,19 @@ function Error(props) {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <p>{error}</p>
-      <p
+    <div className="error--component">
+      <p className="error--detail">{error}</p>
+
+      <button
+        class="error--btn"
+        className="error--btn"
         onClick={() => {
           navigate("/");
         }}
       >
-        Back to Home
-      </p>
+        <span class="error--btn--textOne">Volver a inicio</span>
+        <span class="error--btn--textTwo">Estupendo!</span>
+      </button>
     </div>
   );
 }
